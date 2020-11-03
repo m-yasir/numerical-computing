3e3b73de36988f276bdf0fc82184f554364f391e
Yasir Nadeem <m-yasir.dev@protonmail.com>
2020-11-03 15:20:59 +0500

    Updated false position method to show better info on console. Updated code formatting and style. Best iteration and its intervals are now returned from the function.

--- false_position.py
+++ false_position.py
@@ -1,28 +1,52 @@
-def falsePosition(xl, xu, f, currError = 1, calls = None, iters = 1):
-    if (calls != None and calls <= 0): return (xl, xu)
-    xr = xu-(f(xu)*(xl-xu))/(f(xl)-f(xu))
-    
-    # TODO: Stop if error increases
-    if (currError <= 0.01): # Change fixed error
-        return (xl, xu)
+secantRoot = lambda x0, x1, f: x1-(f(x1)*(x0-x1))/(f(x0)-f(x1))
+
+getApproxError = lambda x_i, x_i_1: abs(x_i_1-x_i)/x_i_1
+
+formatResult = lambda xl, xu, errors, intervals, iters: ((xl, xu), errors, intervals, iters)
+
+def _falseposition(xl, xu, f, currError = 1, calls = None, iters = 1, errors = [], intervals = []) -> (tuple, list, list, int):
+    '''
+        A standard implementation of self thought out (with no optimization in mind) false position iterative method
+        which uses secant method's root finding techinque to calculate next interval's border in a style that is similar to bisection method.
+    '''
+    if (calls != None and calls <= 0): return formatResult(xl, xu, errors, intervals, iters)
+    xr = secantRoot(xl, xu, f)
+
+    if (currError <= 0.01 and prevError < currError): # Change fixed error
+        return formatResult(xl, xu, errors, intervals, iters)
+    intervals.append((xl, xu))
     print("CURRENT INTERVAL: ", (xl, xu), '\n')
     R = f(xl)*f(xu)
     print("XR: ", xr)
     if R < 0:
-        currError = abs(xr-xu)/xr
+        currError = getApproxError(xu, xr)
+        errors.append(currError)
         print(f"#{iters} Current Error: ", currError)
-        return falsePosition(xl, xr, f, currError, calls and calls - 1, iters + 1)
+        return _falseposition(xl, xr, f, currError, calls and calls - 1, iters + 1)
     elif R > 0:
-        currError = abs(xr-xl)/xr
+        currError = getApproxError(xl, xr)
+        errors.append(currError)
         print(f"#{iters} Current Error: ", currError)
-        return falsePosition(xr, xu, f, currError, calls and calls - 1, iters + 1)
+        return _falseposition(xr, xu, f, currError, calls and calls - 1, iters + 1)
     else:
         print(f"#{iters} Current Error: ", currError)
-        return (xl, xu)
+        return formatResult(xl, xu, errors, intervals, iters)
 
-fx = lambda x: ( 5*(x**3) ) - ( 5*(x**2) ) + (6*x) -2
+def falsePosition(xl, xu, f, calls = None):
+    '''
+        Uses false position iteration bracketing method and returns best interval
+    '''
+    _, errors, intervals, it = _falseposition(xl, xu, f, 1, calls)
+    idx = errors.index(min(errors))
+    print()
+    print('*'*10 + ' Final Result ' + '*'*10)
+    print(f'Best Error: {errors[idx]*100:.2f}%')
+    (xl, xu) = intervals[idx]
+    print(f'and its Interval: ({xl:.3f},{xu:.3f}) on iteration: {idx+1}/{it-1}')
+    return intervals[idx]
+
+fx = lambda x: ( 5*(x**3) ) - ( 5*(x**2) ) + (6*x) - 2
 # fx = lambda x: -0.5*(x**2) + 2.5*x + 4.5
 
-print("RESULT: ", falsePosition(0, 1, fx, 1, 4))
-# print("RESULT: ", falsePosition(5, 10, fx, 1, 3))
+falsePosition(0, 1, fx, 4)
     
